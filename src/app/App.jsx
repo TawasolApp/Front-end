92b1ac3f674577d243f37f478f6f4ed581140cca
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-03-20 18:54:37 +0200

    TESTING DEVOPS

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
-import { axiosInstance as axios } from "../apis/axios";
-import { getIconComponent } from "../utils";
-import Companypage from "../pages/CompanyPage/CompanyPage";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+function FirstPage() {
+  return (
+      <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+          <h1 className="text-3xl mr-1">First Page</h1>
+          <Link to="/second" className="text-3xl text-blue-400 underline">Click to go to Second Page</Link>
+      </div>
+  );
+}
+
+function SecondPage() {
+  return (
+      <div className="h-screen flex items-center justify-center bg-gray-900 text-white">
+          <h1 className="text-3xl mr-1">Second Page</h1>
+          <Link to="/" className="text-3xl text-blue-400 underline">Click to go to First Page</Link>
+      </div>
+  );
+}
 
 function App() {
-  const InIcon = getIconComponent("in-black");
   return (
-    <span>
-      <div>
-        <Companypage />
-      </div>
-    </span>
+      <Router>
+          <Routes>
+              <Route path="/" element={<FirstPage />} />
+              <Route path="/second" element={<SecondPage />} />
+          </Routes>
+      </Router>
   );
 }
 
