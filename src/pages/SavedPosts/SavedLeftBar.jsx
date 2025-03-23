667435e21135df052f0d0f240951bfc39e3ba0f3
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-03-24 00:23:52 +0200

    feat: Saved Posts initial with API complete and mock complete

---
+++ src/pages/SavedPosts/SavedLeftBar.jsx
@@ -0,0 +1,11 @@
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+const SavedLeftBar = () => {
+    return (
+        <aside className="w-full">
+            <h1>hllo</h1>
+        </aside>
+    );
+};
+
+export default SavedLeftBar;
---
+++ src/pages/SavedPosts/SavedPosts.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { axiosInstance } from '../../apis/axios';
+import PostCard from '../Feed/MainFeed/FeedPosts/PostCard/PostCard';
+
+const SavedPosts = () => {
+
+    const [localPosts, setLocalPosts] = useState([]);
+
+    const fetchSavedPosts = async () => {
+
+        try {
+            const response = await axiosInstance.get("/posts/saved");
+            setLocalPosts(response.data);
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+
+    const deletePost = async (postId) => {
+        try {
+            await axiosInstance.delete(`/delete/${postId}`);
+            setLocalPosts(prevPosts => prevPosts.filter(post => post.id !== postId));
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchSavedPosts();
+    }, [])
+
+    return (
+        <>
+            {localPosts && localPosts.length > 0 && localPosts.map(post => {
+                return (
+                    <div key={post.id}>
+                        <PostCard post={post} deletePost={deletePost} />
+                    </div>
+                )
+            })}
+            {localPosts.length === 0 && (<h1>Hello world</h1>)}
+        </>
+    );
+}
+
+export default SavedPosts;
