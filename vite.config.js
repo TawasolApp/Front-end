48db8a188fba1793ca61319d2cf78e049385f5b8
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-03-24 05:52:57 +0200

    config: added new configurations for vitest

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    environment: 'jsdom', // Ensures DOM methods are available
+    globals: true         // Enables `describe`, `test`, etc. globally
+  }
 });
