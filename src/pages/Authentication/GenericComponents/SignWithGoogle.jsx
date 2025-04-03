ff65c8a788a0b3d742e263bed9e2cad51eed1f92
KhalidAli44 <khalid4abdelwahab@gmail.com>
2025-04-03 17:49:36 +0200

    feat: added functionality to social login button

--- src/pages/Authentication/GenericComponents/SignWithGoogle.jsx
+++ src/pages/Authentication/GenericComponents/SignWithGoogle.jsx
@@ -1,11 +1,87 @@
+import { useEffect, useRef } from "react";
 import { getIconComponent } from "../../../utils";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  setToken,
+  setRefreshToken,
+  setUserId,
+  setEmail,
+  setFirstName,
+  setLastName,
+  setLocation,
+  setBio,
+  setType,
+  setPicture,
+} from "../../../store/authenticationSlice";
+import { axiosInstance } from "../../../apis/axios";
 
 const SignWithGoogle = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const GoogleGIcon = getIconComponent("google-g");
+  const googleClient = useRef(null);
+  const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-  // TODO: implement social login
+  useEffect(() => {
+    if (window.google) {
+      googleClient.current = window.google.accounts.oauth2.initTokenClient({
+        client_id: googleClientId,
+        scope: "email profile openid",
+        callback: async (tokenResponse) => {
+          if (tokenResponse?.access_token) {
+            try {
+              const response = await axiosInstance.post(
+                "/auth/social-login/google",
+                {
+                  idToken: tokenResponse.access_token,
+                }
+              );
+
+              if (response.status === 200) {
+                const { userId, token, refreshToken } = response.data;
+
+                dispatch(setUserId(userId));
+                dispatch(setToken(token));
+                dispatch(setRefreshToken(refreshToken));
+
+                const profileResponse = await axiosInstance.get(
+                  `/profile/${userId}`
+                );
+
+                if (profileResponse.status === 200) {
+                  const { email, firstName, lastName, location, bio, type, picture } =
+                    profileResponse.data;
+
+                  dispatch(setEmail(email));
+                  dispatch(setFirstName(firstName));
+                  dispatch(setLastName(lastName));
+                  dispatch(setLocation(location));
+                  dispatch(setBio(bio));
+                  dispatch(setType(type));
+                  dispatch(setPicture(picture));
+
+                  navigate("/feed");
+                }
+              }
+            } catch (error) {
+              console.error("Google login failed:", error);
+            }
+          }
+        },
+      });
+    }
+  }, []);
+
+  const handleGoogleLogin = () => {
+    if (googleClient.current) {
+      googleClient.current.requestAccessToken();
+    }
+  };
+
   return (
     <button
+      onClick={handleGoogleLogin}
       type="button"
       className="
         w-full flex items-center justify-center gap-3
