475c2e02cb26b845b363a3d90490f86806c21edb
MarwannAhmed <133513976+MarwannAhmed@users.noreply.github.com>
2025-04-13 20:25:39 +0200

    e2e: login test with incorrect credentials

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -6,9 +6,6 @@ describe('login', () => {
             user = users.userNatalia;
         });
         cy.visit('http://localhost:5173');
-    });
-    
-    it ('Displays login form', () => {
 
         // Click on login button
         cy.getUpperLoginButton().click();
@@ -20,6 +17,9 @@ describe('login', () => {
         cy.getPasswordTextbox().should('exist');
         cy.getForgotPasswordLink().should('have.text', 'Forgot password?');
         cy.getLoginFormButton().should('exist');
+    });
+    
+    it ('Logs in successfully', () => {
 
         // Enter valid credentials
         cy.getEmailTextbox().type(user.email);
@@ -30,5 +30,57 @@ describe('login', () => {
 
         // Assert that feed is now displayed
         cy.url().should('include', '/feed');
+    })
+
+    it ('Does not log in due to incorrect email', () => {
+
+        // Enter incorrect credentials (incorrect email)
+        cy.getEmailTextbox().type("incorrect_email@gmail.com");
+        cy.getPasswordTextbox().type(user.password);
+        
+        // Click on form button
+        cy.getLoginFormButton().click();
+
+        // Assert login failure
+        cy.url().should('include', '/auth/signin');
+        cy.get('.text-red-500').should('have.text', 'Invalid email or password.');
+    })
+
+    it ('Does not log in due to incorrect password', () => {
+
+        // Enter incorrect credentials (incorrect email)
+        cy.getEmailTextbox().type(user.email);
+        cy.getPasswordTextbox().type("incorrect_password");
+        
+        // Click on form button
+        cy.getLoginFormButton().click();
+
+        // Assert login failure
+        cy.url().should('include', '/auth/signin');
+        cy.get('.text-red-500').should('have.text', 'Invalid email or password.');
+    })
+
+    it ('Does not log in due to empty email', () => {
+
+        // Enter incorrect credentials (empty email)
+        cy.getPasswordTextbox().type(user.password);
+        
+        // Click on form button
+        cy.getLoginFormButton().click();
+
+        // Assert login failure
+        cy.url().should('include', '/auth/signin');
+    })
+
+    it ('Does not log in due to empty password', () => {
+
+        // Enter incorrect credentials (empty password)
+        cy.getPasswordTextbox().type(user.email);
+        
+        // Click on form button
+        cy.getLoginFormButton().click();
+
+        // Assert login failure
+        cy.url().should('include', '/auth/signin');
     })
 })
