671ed87559335876cd002f851ece098d136c0bc9
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-04-25 03:03:46 +0300

    feat: improved loading page to dynamics

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,17 @@ export default {
         amberTextLight: "rgb(var(--amber-text-light))",
         amberTextDark: "rgb(var(--amber-text-dark))",
       },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
+        'float': 'float 3s ease-in-out infinite',
+        'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite'
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' }
+        }
+      }
     },
   },
   plugins: [],
