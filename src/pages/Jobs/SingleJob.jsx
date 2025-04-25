926f13cb016aa3be509237743f74fd1faab9d5d9
Mohamed Sobh <129511963+MohamedSobh032@users.noreply.github.com>
2025-04-25 21:15:58 +0300

    feat: containerized single job route

--- src/pages/Jobs/SingleJob.jsx
+++ src/pages/Jobs/SingleJob.jsx
@@ -3,9 +3,12 @@ import { useParams } from "react-router-dom";
 
 const SingleJob = () => {
   const { id } = useParams();
+  
   return (
-    <div className="min-h-screen bg-cardBackground">
-      <JobDescription jobId={id} enableReturn={true} />
+    <div className="min-h-screen bg-mainBackground">
+      <div className="mx-0 px-0 md:max-w-3xl md:mx-auto md:px-4 md:py-8">
+        <JobDescription jobId={id} enableReturn={true} />
+      </div>
     </div>
   );
 };
