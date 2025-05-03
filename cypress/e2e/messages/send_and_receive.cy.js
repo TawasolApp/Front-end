14bfb8528c2a2739b347dc4187db0c2232dcaf90
MarwannAhmed <133513976+MarwannAhmed@users.noreply.github.com>
2025-05-03 18:22:51 +0300

    e2e: finalized messaging tests

--- cypress/e2e/messages/send_and_receive.cy.js
+++ cypress/e2e/messages/send_and_receive.cy.js
@@ -1,79 +1,169 @@
 describe("sends and receives messages", () => {
-    let user1;
-    let user2;
-  
-    beforeEach(() => {
+  let user1;
+  let user2;
 
-      cy.visit("http://localhost:5173");
-      cy.getUpperRegistrationButton().click();
-      user1 = {
-        email: "",
-        password: "07032004",
-        firstName: "",
-        lastName: ""
-      };
-      const randomString1 = Math.random().toString(36).substring(2, 10);
-      user1.email = `${randomString1}@mailinator.com`;
-      const inboxName1 = randomString1;
-      user1.firstName = randomString1;
-      user1.lastName = randomString1;
-      cy.register(user1, inboxName1, user1.firstName, user1.lastName);
+  beforeEach(() => {
+    cy.visit("http://localhost:5173");
+    cy.getUpperRegistrationButton().click();
+    user1 = {
+      email: "",
+      password: "07032004",
+      firstName: "",
+      lastName: "",
+    };
+    const randomString1 = Math.random().toString(36).substring(2, 10);
+    user1.email = `${randomString1}@mailinator.com`;
+    const inboxName1 = randomString1;
+    user1.firstName = randomString1;
+    user1.lastName = randomString1;
+    cy.register(user1, inboxName1, user1.firstName, user1.lastName);
 
-      cy.visit("http://localhost:5173");
-      cy.getUpperRegistrationButton().click();
-      user2 = {
-        email: "",
-        password: "07032004",
-        firstName: "",
-        lastName: ""
-      };
-      const randomString2 = Math.random().toString(36).substring(2, 10);
-      user2.email = `${randomString2}@mailinator.com`;
-      const inboxName2 = randomString2;
-      user2.firstName = randomString2;
-      user2.lastName = randomString2;
-      cy.register(user2, inboxName2, user2.firstName, user2.lastName);
-    });
-  
-    it("Sends a message and ensures its receipt on the other side", () => {
-
-      cy.getSearchBar().type(user1.firstName);
-      cy.getSearchBar().type("{enter}");
-      cy.wait(5000);
-
-      cy.getSearchDropDownMenu().click();
-      cy.getSearchDropDownPeople().click();
-      cy.wait(5000);
-
-      cy.getPeopleFirstSearchResult().click();
-      cy.wait(5000);
-
-      cy.getMessageUserButton().click();
-      cy.wait(5000);
-
-      cy.getMessageAreaInput().type("Hello, this is a test message!");
-      cy.getMessageSendButton().click();
-      cy.wait(5000);
-
-      cy.getMessagesPage().click();
-      cy.wait(5000);
-
-      cy.getFirstChat().should("contain", user1.firstName);
-      cy.getFirstChat().click();
-      cy.wait(5000);
-
-      cy.getMessageContent().should("contain", "Hello, this is a test message!");
-      cy.wait(5000);
-
-      cy.login(user1);
-      cy.wait(5000);
-      cy.getMessagesPage().click();
-      cy.wait(5000);
-      cy.getFirstChat().should("contain", user2.firstName);
-      cy.getFirstChat().click();
-      cy.wait(5000);
-      cy.getMessageContent().should("contain", "Hello, this is a test message!");
-
-    });
+    cy.visit("http://localhost:5173");
+    cy.getUpperRegistrationButton().click();
+    user2 = {
+      email: "",
+      password: "07032004",
+      firstName: "",
+      lastName: "",
+    };
+    const randomString2 = Math.random().toString(36).substring(2, 10);
+    user2.email = `${randomString2}@mailinator.com`;
+    const inboxName2 = randomString2;
+    user2.firstName = randomString2;
+    user2.lastName = randomString2;
+    cy.register(user2, inboxName2, user2.firstName, user2.lastName);
   });
-  
+
+  it("Sends a message, checks unseen count and ensures its receipt on the other side", () => {
+    cy.getSearchBar().type(user1.firstName);
+    cy.getSearchBar().type("{enter}");
+    cy.wait(5000);
+
+    cy.getSearchDropDownMenu().click();
+    cy.getSearchDropDownPeople().click();
+    cy.wait(5000);
+
+    cy.getPeopleFirstSearchResult().click();
+    cy.wait(5000);
+
+    cy.getMessageUserButton().click();
+    cy.wait(5000);
+
+    cy.getMessageAreaInput().type("Hello, this is a test message!");
+    cy.getMessageSendButton().click();
+    cy.wait(5000);
+
+    cy.getMessagesPage().click();
+    cy.wait(5000);
+
+    cy.getFirstChat().should("contain", user1.firstName);
+    cy.getFirstChat().click();
+    cy.wait(5000);
+
+    cy.getMessageContent().should("contain", "Hello, this is a test message!");
+    cy.getSentIcon().should("exist");
+    cy.wait(5000);
+
+    cy.login(user1);
+    cy.wait(5000);
+
+    cy.getMessagesPage().click();
+    cy.wait(5000);
+
+    cy.login(user2);
+    cy.wait(5000);
+
+    cy.getMessagesPage().click();
+    cy.wait(5000);
+
+    cy.getFirstChat().click();
+    cy.wait(5000);
+
+    cy.getDeliveredIcon().should("exist");
+
+    cy.login(user1);
+    cy.wait(5000);
+
+    cy.getMessagesPage().click();
+    cy.wait(5000);
+
+    cy.getFirstChat().should("contain", user2.firstName);
+    cy.getUnseenMessagesCount().should("contain", "1");
+    cy.getFirstChat().click();
+    cy.wait(5000);
+
+    cy.getMessageContent().should("contain", "Hello, this is a test message!");
+
+    cy.getSenderAvatar().trigger('mouseover');
+    cy.wait(3000);
+    cy.getHoveredSenderAvatar().click();
+    cy.wait(5000);
+
+    cy.getMarkAsReadButton().click();
+    cy.wait(5000);
+
+    cy.getUnseenMessagesCount().should("not.exist");
+
+    cy.getSenderAvatar().trigger('mouseover');
+    cy.wait(3000);
+    cy.getHoveredSenderAvatar().click();
+    cy.wait(5000);
+
+    cy.getMarkAsUnreadButton().click();
+    cy.wait(5000);
+
+    cy.getUnseenMessagesCount().should("exist");
+
+    cy.login(user2);
+    cy.wait(5000);
+
+    cy.getMessagesPage().click();
+    cy.wait(5000);
+
+    cy.getFirstChat().click();
+    cy.wait(5000);
+
+    cy.getReadIcon().should("exist");
+
+  });
+
+  // it.skip("Sends media and ensures its receipt on the other side", () => {
+  //   cy.getSearchBar().type(user1.firstName);
+  //   cy.getSearchBar().type("{enter}");
+  //   cy.wait(5000);
+
+  //   cy.getSearchDropDownMenu().click();
+  //   cy.getSearchDropDownPeople().click();
+  //   cy.wait(5000);
+
+  //   cy.getPeopleFirstSearchResult().click();
+  //   cy.wait(5000);
+
+  //   cy.getMessageUserButton().click();
+  //   cy.wait(5000);
+
+  //   cy.getSendImageButton().selectFile("cypress/fixtures/imgs/polar_bear.png", { force: true });
+  //   cy.wait(10000);
+  //   cy.getMessageSendButton().click();
+  //   cy.wait(5000);
+
+  //   cy.getMessagesPage().click();
+  //   cy.wait(5000);
+
+  //   cy.getFirstChat().should("contain", user1.firstName);
+  //   cy.getFirstChat().click();
+  //   cy.wait(5000);
+
+  //   cy.getSentImageSrc().should("include", "polar_bear.png");
+  //   cy.wait(5000);
+
+  //   cy.login(user1);
+  //   cy.wait(5000);
+  //   cy.getMessagesPage().click();
+  //   cy.wait(5000);
+  //   cy.getFirstChat().should("contain", user2.firstName);
+  //   cy.getFirstChat().click();
+  //   cy.wait(5000);
+  //   cy.getSentImageSrc().should("include", "polar_bear.png");
+  // });
+});
--- cypress/support/messages.js
+++ cypress/support/messages.js
@@ -1,31 +1,75 @@
-Cypress.Commands.add('getMessagesPage', () => {
-    return cy.get('[data-testid="navbar-Messaging"]');
+Cypress.Commands.add("getMessagesPage", () => {
+  return cy.get('[data-testid="navbar-Messaging"]');
 });
 
 Cypress.Commands.add("getSearchDropDownPeople", () => {
-    return cy.get('[data-testid="SearchDropdownOption-people"]');
-  });
+  return cy.get('[data-testid="SearchDropdownOption-people"]');
+});
 
-  Cypress.Commands.add("getPeopleFirstSearchResult", () => {
-    return cy.get('[data-testid="searchUserName"]');
-  });
+Cypress.Commands.add("getPeopleFirstSearchResult", () => {
+  return cy.get('[data-testid="searchUserName"]');
+});
 
-  Cypress.Commands.add("getMessageUserButton", () => {
-    return cy.get('.bg-blue-600');
-  });
+Cypress.Commands.add("getMessageUserButton", () => {
+  return cy.get(".bg-blue-600");
+});
 
-  Cypress.Commands.add("getMessageAreaInput", () => {
-    return cy.get('.p-3 > .w-full');
-  });
+Cypress.Commands.add("getMessageAreaInput", () => {
+  return cy.get(".p-3 > .w-full");
+});
 
-  Cypress.Commands.add("getMessageSendButton", () => {
-    return cy.get('.justify-between > .px-3');
-  });
+Cypress.Commands.add("getMessageSendButton", () => {
+  return cy.get(".justify-between > .px-3");
+});
 
-  Cypress.Commands.add("getFirstChat", () => {
-    return cy.get('.w-2\\/5 > :nth-child(1) > :nth-child(1) > .items-start > .flex-1 > .justify-between > .py-1 > .font-medium');
-  });
+Cypress.Commands.add("getFirstChat", () => {
+  return cy.get(
+    ".w-2\\/5 > :nth-child(1) > :nth-child(1) > .items-start > .flex-1 > .justify-between > .py-1 > .font-medium"
+  );
+});
 
-  Cypress.Commands.add("getMessageContent", () => {
-    return cy.get('.w-3\\/5 > .flex-col > .flex-1 > .max-w-xs > :nth-child(1)');
-  });
+Cypress.Commands.add("getMessageContent", () => {
+  return cy.get(".w-3\\/5 > .flex-col > .flex-1 > .max-w-xs > :nth-child(1)");
+});
+
+Cypress.Commands.add("getSendImageButton", () => {
+  return cy.get('[title="Add image"] input[type="file"]');
+});
+
+Cypress.Commands.add("getSentImageSrc", () => {
+  return cy.get('img[alt="Message media"]').invoke("attr", "src");
+});
+
+Cypress.Commands.add("getUnseenMessagesCount", () => {
+  return cy.get(
+    ".w-2\\/5 > :nth-child(1) > :nth-child(1) > .items-start > .flex-1 > .justify-between > .flex-col > .flex"
+  );
+});
+
+Cypress.Commands.add("getSenderAvatar", () => {
+  return cy.get(".mr-3 > .MuiAvatar-root");
+});
+
+Cypress.Commands.add("getHoveredSenderAvatar", () => {
+  return cy.get('.mr-3 > .absolute');
+});
+
+Cypress.Commands.add("getMarkAsReadButton", () => {
+  return cy.get('[aria-label="Mark as read"]');
+});
+
+Cypress.Commands.add("getMarkAsUnreadButton", () => {
+  return cy.get('[aria-label="Mark as unread"]');
+});
+
+Cypress.Commands.add("getSentIcon", () => {
+  return cy.get('[data-testid="DoneIcon"]');
+});
+
+Cypress.Commands.add("getDeliveredIcon", () => {
+  return cy.get('.w-3\\/5 > .flex-col > .flex-1 > .max-w-xs > .flex > [title="Delivered"] > [data-testid="DoneAllIcon"]');
+});
+
+Cypress.Commands.add("getReadIcon", () => {
+  return cy.get('.w-3\\/5 > .flex-col > .flex-1 > .max-w-xs > .flex > [title="Read"] > [data-testid="DoneAllIcon"]');
+});
