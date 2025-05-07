1c92e5630655f377f950ad98b9021f709dc41e13
yousefsawy <99139949+yousefsawy@users.noreply.github.com>
2025-05-07 04:20:13 +0300

    Fix e2e/notifications

--- cypress/e2e/notifications/notifications.cy.js
+++ cypress/e2e/notifications/notifications.cy.js
@@ -6,7 +6,18 @@ describe('open news feed', () => {
       cy.visit('http://localhost:5173/feed');
     });
 
-    // SHOULD BE MOCKED
+    // SHOULD SEED TO PASS
+
+    describe('View Notifcations initally', () => {
+        it('View Notifications after logging in alphonso', () => {
+            cy.fixture('users.json').then((users) => {
+                cy.login(users.userAlphonso);
+              });
+              cy.visit('http://localhost:5173/notifications');
+              cy.get('.flex > .MuiBadge-root > .MuiBadge-badge').should('have.text', '5'); //Initially 5 notifications
+            });
+    });
+
 
     describe('Notifications Creation', () => {
         it('Open Alphonso Profile and trigger notifications', () => {
@@ -26,13 +37,14 @@ describe('open news feed', () => {
         });
     });
 
-    describe('View Notifcations', () => {
+    // BUG: Like notifications are not being created
+    describe('View Notifcations after like/comment/connect', () => {
         it('View Notifications after logging in alphonso', () => {
             cy.fixture('users.json').then((users) => {
                 cy.login(users.userAlphonso);
               });
               cy.visit('http://localhost:5173/notifications');
-              cy.get('.flex > .MuiBadge-root > .MuiBadge-badge').should('have.text', '8');
+              cy.get('.flex > .MuiBadge-root > .MuiBadge-badge').should('have.text', '8'); // The number of notifications should be 8
             });
     });
 
@@ -42,10 +54,21 @@ describe('open news feed', () => {
             cy.getSearchDropDownMenu().click();
             cy.getSearchDropdownPeople().click();
             cy.getSearchPeople().first().click();
-            cy.get('.capitalize').first().click(); // Send connection request
+            cy.get('.capitalize').first().click(); // Remove connection request
             cy.wait(1000);
             cy.get('[data-testid="main-like-button"]').first().click(); // Click on the like
             cy.wait(1000);
+        });
+    });
+
+
+    describe('View Notifcations after removal', () => {
+        it('View Notifications after logging in alphonso', () => {
+            cy.fixture('users.json').then((users) => {
+                cy.login(users.userAlphonso);
+              });
+              cy.visit('http://localhost:5173/notifications');
+              cy.get('.flex > .MuiBadge-root > .MuiBadge-badge').should('have.text', '8'); // The number of notifications remain 8
         });
     });
 
@@ -55,7 +78,13 @@ describe('open news feed', () => {
                 cy.login(users.userAlphonso);
               });
               cy.visit('http://localhost:5173/notifications');
-              cy.get('.flex > .MuiBadge-root > .MuiBadge-badge').should('have.text', '7');
+              cy.get('.divide-y > :nth-child(1)').click(); // Click on the first notification
+              cy.visit('http://localhost:5173/notifications');
+              cy.get('.divide-y > :nth-child(2)').click(); // Click on the first notification
+              cy.visit('http://localhost:5173/notifications');
+              cy.get('.divide-y > :nth-child(3)').click(); // Click on the first notification
+              cy.visit('http://localhost:5173/notifications');
+              cy.get('.flex > .MuiBadge-root > .MuiBadge-badge').should('have.text', '5'); // The number of notifications should be 5
         });
     });
 
